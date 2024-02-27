a66326123b234d44f9789172d407fe83eb83409d
Alex <alexvnep@gmail.com>
2024-02-27 16:04:14 +0200

    'Add addBookToLibrary func that takes newBook obj data  and adds it to  myLibrary array. Current issue is when book is added it prints out new book plus what was added on multiple lines'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary =  ['The Hobbit', 'Eragon', 'Brisingr'];
+const myLibrary =  ['The hobbit'];
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -10,26 +10,45 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary (){
-    // I want to take the input from the user and store the new book objects into an array.
-    // 
+const  userInput = document.querySelectorAll('input');
+const addBookBtn = document.querySelector('.add-book');
+
+
+
+const addBookToLibrary = (ev) => {
+    ev.preventDefault(); // ev is the event being passed to the function here it is preventing the btn from submitting
+    let newBook = {
+        title: document.querySelector('#title').value,
+        author: document.querySelector('#author').value,
+        pages: document.querySelector('#pages').value,
+        yes: document.querySelector('#yes').value,
+        no: document.querySelector('#no').value,
+    }
+    myLibrary.push(newBook);
+    document.forms[0].reset();
+
+    console.warn('added', {myLibrary});
+    const bookList = document.querySelector('.books');
+    const bookListContent = document.createElement('div');
+    bookListContent.textContent = '\n' + JSON.stringify(myLibrary, '\t', 2)
+    bookList.appendChild(bookListContent); 
 }
+addBookBtn.addEventListener('click', addBookToLibrary);
 
 
-const bookList = document.querySelector('.books');
 
-function displayBooks(){
-    for (let i = 0; i < myLibrary.length; i++) {
-        const bookListContent = document.createElement('div');
-        let book = myLibrary[i];
-        bookListContent.textContent = book
-        bookList.appendChild(bookListContent);        
-    }   
-}    
-displayBooks();
+// function displayBooks(){
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         const bookListContent = document.createElement('div');
+//         let book = myLibrary[i];
+//         bookListContent.textContent = book
+//         bookList.appendChild(bookListContent);        
+//     }   
+// }    
+// displayBooks();
 
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not read yet');
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'no');
 
 theHobbit.info(); 
 console.log(theHobbit.info());
